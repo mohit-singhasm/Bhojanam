dd8a3caee9ebce535e55aac207cf8b90dc7c8782
Mohit Singh <137118917+mohit-singhasm@users.noreply.github.com>
2024-01-29 07:33:52 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="border-t border-gray-300 mt-16">
         <div className="container text-center text-gray-500 py-4">
-        &copy; Copyright @The Brave Coders 2023.
+        &copy; Copyright @Mohit 2024.
         </div>
     </div>
   )
